10861607e4deaa39549a773025a3306a7b0adaee
Justincee <calvertjustin1996@gmail.com>
2021-06-18 14:23:40 +0200

    updated third screen

--- main3.py
+++ main3.py
@@ -1,5 +1,9 @@
 import tkinter
 from tkinter import *
+from tkinter.ttk import Combobox
+from tkinter import messagebox
+
+import requests
 
 window = Tk()
 window.title('Claim Prize')
@@ -28,6 +32,38 @@ bank_menu.place(x=300, y=210)
 bank_option = Label(window, text='Please select your bank:', bg='yellow')
 bank_option.place(x=5, y=210)
 
+amount = IntVar()
+response = requests.get('https://v6.exchangerate-api.com/v6/cf719f6bfba5177bc04a822b/latest/ZAR')
+response_j = response.json()
 
+conv_rates = response_j['conversion_rates']
+
+curr_options = []
+for i in conv_rates.keys():
+    curr_options.append(i)
+
+currency_combo = Combobox(window)
+currency_combo['values'] = curr_options
+currency_combo['state'] = 'readonly'
+currency_combo.set('Select foreign currency')
+currency_combo.place(x=5, y=360)
+
+
+def currency_convert():
+    number = float(winnings_entry.get())
+    messagebox.showinfo("money", response_j['conversion_rates'][currency_combo.get()])
+    answer = number * response_j['conversion_rates'][currency_combo.get()]
+    convert_result['text'] = answer
+
+
+winnings = Label(window, text='Enter Amount Won', bg='yellow')
+winnings.place(x=5, y=300)
+winnings_entry = Entry(window)
+winnings_entry.place(x=300, y=300)
+convert_result = Label(window, text='Result after conversion: ', bg='yellow')
+convert_result.place(x=5, y=420)
+
+convert_zar = Button(window, text='Convert amount', command=currency_convert)
+convert_zar.place(x=300, y=360)
 
 window.mainloop()
